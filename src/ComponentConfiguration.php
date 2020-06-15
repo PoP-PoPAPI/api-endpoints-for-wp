df44a44c34629d2bf5589b2551f2798023031d38
Leonardo Losoviz <leo@getpop.org>
2020-06-15 15:53:50 +0800

    GraphQL goes to own package, REST discarded

--- src/ComponentConfiguration.php
+++ src/ComponentConfiguration.php
@@ -12,84 +12,8 @@ class ComponentConfiguration
 {
     use ComponentConfigurationTrait;
 
-    private static $isGraphQLAPIEndpointDisabled;
-    private static $getGraphQLAPIEndpoint;
-    private static $isRESTAPIEndpointDisabled;
-    private static $getRESTAPIEndpoint;
     private static $isNativeAPIEndpointDisabled;
     private static $getNativeAPIEndpoint;
-
-    public static function isGraphQLAPIEndpointDisabled(): bool
-    {
-        // Define properties
-        $envVariable = Environment::DISABLE_GRAPHQL_API_ENDPOINT;
-        $selfProperty = &self::$isGraphQLAPIEndpointDisabled;
-        $defaultValue = false;
-        $callback = [EnvironmentValueHelpers::class, 'toBool'];
-
-        // Initialize property from the environment/hook
-        self::maybeInitializeConfigurationValue(
-            $envVariable,
-            $selfProperty,
-            $defaultValue,
-            $callback
-        );
-        return $selfProperty;
-    }
-
-    public static function getGraphQLAPIEndpoint(): string
-    {
-        // Define properties
-        $envVariable = Environment::GRAPHQL_API_ENDPOINT;
-        $selfProperty = &self::$getGraphQLAPIEndpoint;
-        $defaultValue = '/api/graphql/';
-        $callback = [EndpointUtils::class, 'slashURI'];
-
-        // Initialize property from the environment/hook
-        self::maybeInitializeConfigurationValue(
-            $envVariable,
-            $selfProperty,
-            $defaultValue,
-            $callback
-        );
-        return $selfProperty;
-    }
-
-    public static function isRESTAPIEndpointDisabled(): bool
-    {
-        // Define properties
-        $envVariable = Environment::DISABLE_REST_API_ENDPOINT;
-        $selfProperty = &self::$isRESTAPIEndpointDisabled;
-        $defaultValue = false;
-        $callback = [EnvironmentValueHelpers::class, 'toBool'];
-
-        // Initialize property from the environment/hook
-        self::maybeInitializeConfigurationValue(
-            $envVariable,
-            $selfProperty,
-            $defaultValue,
-            $callback
-        );
-        return $selfProperty;
-    }
-
-    public static function getRESTAPIEndpoint(): string
-    {
-        // Define properties
-        $envVariable = Environment::REST_API_ENDPOINT;
-        $selfProperty = &self::$getRESTAPIEndpoint;
-        $defaultValue = '/api/rest/';
-        $callback = [EndpointUtils::class, 'slashURI'];
-
-        // Initialize property from the environment/hook
-        self::maybeInitializeConfigurationValue(
-            $envVariable,
-            $selfProperty,
-            $defaultValue,
-            $callback
-        );
-        return $selfProperty;
-    }
 
     public static function isNativeAPIEndpointDisabled(): bool
     {
