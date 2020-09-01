f680c4747244247c13f83103e99907d79df6d5cc
Leonardo Losoviz <leo@getpop.org>
2020-09-02 06:13:09 +0800

    Added typehints

--- src/Component.php
+++ src/Component.php
@@ -18,6 +18,11 @@ class Component extends AbstractComponent
 
     // const VERSION = '0.1.0';
 
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return string[]
+     */
     public static function getDependedComponentClasses(): array
     {
         return [
@@ -27,6 +32,9 @@ class Component extends AbstractComponent
 
     /**
      * Initialize services
+     *
+     * @param mixed[] $configuration
+     * @param string[] $skipSchemaComponentClasses
      */
     protected static function doInitialize(
         array $configuration = [],
